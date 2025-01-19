2112fcf9587dc2e3a4ff9ef0aec7be354122589d
mewan-rathnayaka <mcrlgdous@gmail.com>
2025-01-19 18:52:33 +0530

    inital commit

--- src/main/scala/cache_phase2/constants.scala
+++ src/main/scala/cache_phase2/constants.scala
@@ -24,13 +24,13 @@ object constantsDCache{
   val FIFO_ADDR_TX = "hE000_1030"
   val FIFO_ADDR_RX = "hE000_102C"
 
-  val dPort_ID : Int = 1
+  // val dPort_ID : Int = 1
   val dPort_PROT : Int = 2
   val dPort_LEN : Int = 7     //= "b0000_0001"         //"b0000_0111"
   val dPort_SIZE : Int = 3       //= "b010"        //"b011"
   val dPort_WIDTH: Int = math.pow(2, dPort_SIZE).toInt * 8  // 64 //32
 
-  val peripheral_ID : Int = 1
+  // val peripheral_ID : Int = 1
   val peripheral_LEN : Int = 1        //= "b0000_0001"
   val peripheral_SIZE : Int = 2       //= "b010"
   val peripheral_WIDTH : Int = math.pow(2, peripheral_SIZE).toInt * 8//32      //64
